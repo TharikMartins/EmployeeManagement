43f7ab20bfb6357b628ce0d8f401b66b1ae332b7
Tharik Martins <tharikmartins@hotmail.com>
2021-07-17 03:11:48 -0300

    Iniciando projeto de gestão de funcionários

---
+++ src/Infrastructure.Repository/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using Management.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure.Repository
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Employee>().ToTable("Employee");
+        }
+    }
+}
---
+++ src/Management.Domain/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Management.Domain
+{
+    public class Employee
+    {
+
+        public Employee(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("Name cannot be null or empty.");
+
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
